1b8137bdd6e04882e144a537537754d733bdc5bd
ahmedaryf <ahmed.areef@hotmail.com>
2023-07-12 00:20:10 +0500

    Home page text color changed

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        <h1 className='text-center'>Home Page</h1>
+        <h1 className='text-center mt-5 text-danger'>Home Page</h1>
       </div>
     </main>
   )
