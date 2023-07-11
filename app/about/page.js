cb854f63fa588b2ccb9b2469f2ab6577384f4d7b
ahmedaryf <ahmed.areef@hotmail.com>
2023-07-12 00:10:19 +0500

    initial commit

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export const metadata = {
+    title: 'About Page Title'
+}
+
+function About() {
+  return (
+    <div>
+        <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default About
